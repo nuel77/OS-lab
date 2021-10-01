4358be33e4563a1bbf1acf2328a81aa93bd48326
Emmanuel Thomas <50878033+nuel77@users.noreply.github.com>
2021-10-01 10:14:59 +0530

    refractored stage 14

--- commands/start.sh
+++ commands/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ../spl
 echo OSSTARTUP COMPILING ...
-./spl spl_progs/os_startup_stage14.spl
+./spl spl_progs/os_startup.spl
 echo TIMER INTERRUPT COMPILING ...
-./spl spl_progs/timer_interrupt_stage14.spl
+./spl spl_progs/timer_interrupt.spl
 echo INT10 COMPILING ...
-./spl spl_progs/int10_stage14.spl
+./spl spl_progs/int10.spl
 echo BOOTMODULE COMPILING ...
-./spl spl_progs/bootmodule_stage14.spl
+./spl spl_progs/bootmodule.spl
 echo MOD5 COMPILING ...
 ./spl spl_progs/mod5.spl
 cd ../xfs-interface
 ./xfs-interface fdisk
-./xfs-interface run ../commands/stage_14.txt
+./xfs-interface run ../commands/current.txt
